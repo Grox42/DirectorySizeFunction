142c04f593ac7490d9b3f6fede31c9a1f7cca9b5
Yudin Roman <r.v.yudin@viarune.com>
2024-05-18 14:17:33 +0700

    switched to the strategy pattern

---
+++ DirSizeMapper.h
@@ -0,0 +1,16 @@
+#ifndef DIRSIZEMAPPER_H
+#define DIRSIZEMAPPER_H
+
+#include "ISizeMapper.h"
+#include <QFileInfo>
+
+class DirSizeMapper : public ISizeMapper
+{
+private:
+    quint64 getSize(const QFileInfo& fileInfo) const;
+public:
+    virtual ~DirSizeMapper() override = default;
+    virtual QMap<QString, quint64>& getSizesMap(const QDir& dir) const override;
+};
+
+#endif // DIRSIZEMAPPER_H
--- main.cpp
+++ main.cpp
@@ -1,48 +1,18 @@
-#include <QCoreApplication>
-#include <QString>
-#include <QPair>
+#include "ISizeMapper.h"
+#include "DirSizeMapper.h"
 #include <QDir>
-#include <QFileInfo>
-#include <QFileInfoList>
+#include <QMap>
+#include <QPair>
 #include <QDebug>
 
-enum class Group
+int main()
 {
-    ByFolders, ByTypes
-};
-
-qint64 getSizeEntry(const QFileInfo &fileInfo)
-{
-    if (fileInfo.isDir()) {
-        QDir dir = fileInfo.dir();
-        dir.cd(fileInfo.fileName());
-
-        qint64 sum = 0;
-        foreach (QFileInfo entry, dir.entryInfoList(QDir::AllEntries | QDir::NoDotAndDotDot))
-            sum += getSizeEntry(entry);
-
-        return sum;
-    } else return fileInfo.size();
-}
-
-QPair<QString, qint64> getDirInfo(const QDir &dir, Group group)
-{
-    switch (group) {
-        case Group::ByFolders: return QPair<QString, qint64>(dir.path(), getSizeEntry(QFileInfo(dir.path())));
-        case Group::ByTypes:
-
-            return QPair<QString, qint64>();
-    }
-}
-
-
-int main(int argc, char *argv[])
-{
-    QCoreApplication a(argc, argv);
-
     QDir rootDir("Dir");
+    ISizeMapper* mapper = new DirSizeMapper();
+    QMap<QString, quint64> map = mapper->getSizesMap(rootDir);
 
-    qDebug() << getDirInfo(rootDir, Group::ByFolders);
+    foreach (const QString& key, map.keys())
+        qDebug() << key << map.value(key);
 
-    return a.exec();
+    return 0;
 }
